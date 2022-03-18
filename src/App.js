4c33306f049a8507083b9575cd015454b8c8e935
LazezBZH <lazez.bzh@caramail.fr>
2022-03-18 17:54:17 +0100

    V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation, Trans } from "react-i18next";
+import moi from "./assets/moi.jpg";
 
-function App() {
+export default function App() {
+  const { t: translate, i18n } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App-header">
+        <h2>{translate("Welcome")}</h2>
+        <button
+          style={{
+            backgroundColor: i18n.resolvedLanguage === "fr" ? "white" : "black",
+            fontWeight: i18n.resolvedLanguage === "fr" ? "bolder" : "normal",
+            color: i18n.resolvedLanguage === "fr" ? "black" : "white",
+            borderColor: i18n.resolvedLanguage === "fr" ? "red" : "black",
+          }}
+          onClick={() => changeLanguage("fr")}
         >
-          Learn React
-        </a>
-      </header>
+          Français
+        </button>
+        <button
+          style={{
+            backgroundColor: i18n.resolvedLanguage === "en" ? "white" : "black",
+            fontWeight: i18n.resolvedLanguage === "en" ? "bolder" : "normal",
+            color: i18n.resolvedLanguage === "en" ? "black" : "white",
+            borderColor: i18n.resolvedLanguage === "en" ? "red" : "black",
+          }}
+          onClick={() => changeLanguage("en")}
+        >
+          English
+        </button>
+      </div>
+      <div className="app-welcome">
+        <div className="defil">
+          <Trans i18nKey="welcome">trans</Trans>
+        </div>
+        <div div className="moi">
+          <img className="moi-img" src={moi} alt="moi" />
+        </div>
+      </div>
+
+      <div style={{ marginTop: 40 }}>
+        Learn more:&nbsp;
+        <a href="https://react.i18next.com">https://react.i18next.js</a>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/i18n.js
@@ -0,0 +1,44 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    // we init with resources
+    resources: {
+      en: {
+        translations: {
+          "To get started, edit <1>src/App.js</1> and save to reload.":
+            "To get started, edit <1>src/App.js</1> and save to reload.",
+          Welcome:
+            "Hey, I'm Bénédicte HÉRAULT, loocking for my first job as a front-end dev!",
+          welcome: "Hello World!",
+        },
+      },
+      fr: {
+        translations: {
+          "To get started, edit <1>src/App.js</1> and save to reload.":
+            "Starte in dem du, <1>src/App.js</1> editierst und speicherst.",
+          Welcome:
+            "Bonjour, je suis Bénédicte HÉRAULT et je recherche mon premier emploi en tant que développeuse front-end!",
+          welcome: "Coucou tout le monde!",
+        },
+      },
+    },
+    fallbackLng: "en",
+    debug: true,
+
+    // have a common namespace used around the full app
+    ns: ["translations"],
+    defaultNS: "translations",
+
+    keySeparator: false, // we use content as keys
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
